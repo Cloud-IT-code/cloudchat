380222abac63d14312a75d0fbd11b4fe6d7c903a
Cloud-IT-code <samehikari@gmail.com>
2021-02-16 04:19:56 +0700

    make it better

--- src/widget/default-configuration.js
+++ src/widget/default-configuration.js
@@ -1,15 +1,15 @@
 
 export const defaultConfiguration = {
-    titleClosed: 'Click to chat!',
-    titleOpen: 'Let\'s chat!',
-    closedStyle: 'chat', // button or chat
-    closedChatAvatarUrl: '', // only used if closedStyle is set to 'chat'
-    cookieExpiration: 1, // in days. Once opened, closed chat title will be shown as button (when closedStyle is set to 'chat')
-    introMessage: 'Hello! How can we help you?',
-    autoResponse: 'Looking for the first available admin (It might take a minute)',
-    autoNoResponse: 'It seems that no one is available to answer right now. Please tell us how we can ' +
-    'contact you, and we will get back to you as soon as we can.',
-    placeholderText: 'Send a message...',
+    titleClosed: 'Klik untuk mengobrol!',
+    titleOpen: 'Mari berbincang!',
+    closedStyle: 'chat', // button atau chat
+    closedChatAvatarUrl: '', // hanya digunakan jika closedStyle disetel ke 'chat'
+    cookieExpiration: 1, // dalam beberapa hari. Setelah dibuka, judul obrolan tertutup akan ditampilkan sebagai tombol (saat closedStyle disetel ke'chat')
+    introMessage: 'Halo! Bagaimana kami dapat membantu Anda?',
+    autoResponse: 'Mencari admin pertama yang tersedia (Mungkin perlu beberapa menit)',
+    autoNoResponse: 'Sepertinya tidak ada yang bisa menjawab saat ini. Tolong beritahu kami bagaimana kami bisa ' +
+    'menghubungi Anda, dan kami akan menghubungi Anda sesegera mungkin.',
+    placeholderText: 'Kirim pesan...',
     displayMessageTime: true,
     mainColor: '#1f8ceb',
     alwaysUseFloatingButton: false,
